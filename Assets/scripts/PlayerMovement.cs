90103c655cbf5845f8e3daf1d483df1b69938c74
Ray <ray.hancock.c@gmail.com>
2018-12-17 23:24:53 -0500

    fixed NaN error

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -23,7 +23,7 @@ public class PlayerMovement : MonoBehaviour {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-        if(rb.position.y < -1f)
+        if(rb.position.y < -2f)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
